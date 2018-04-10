08f66f35a29d3ca7b1304f3cccfb145128ae4e2d
gnrlpz <gener.lopez@gmail.com>
2018-04-10 23:19:32 +0800

    added room8,9

--- DragonMazeRunner/src/room/Room8.java
+++ DragonMazeRunner/src/room/Room8.java
@@ -1,5 +1,86 @@
 package room;
 
-public class Room8 {
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
+import anno.Command;
+import anno.Direction;
+import maze.MazeMaker;
+
+public class Room8 {
+	@Direction(command="edgeworthOffice")
+	private Room7 room7;
+	@Direction(command="303")
+	private Room9 room9;
+	
+	private boolean usedButton, sprayedButtonWithLuminol;
+	
+	private String[] dialogue;
+	private int dialogueCount;
+	public boolean isDialogueFinished;
+	
+	public Room8() {
+		dialogue = new String[3];
+		dialogueCount = 0;
+		isDialogueFinished = false;
+
+		dialogue[0] = "Phoenix: There\'s quite a lot of documents here. There seems to be a stack related to the current case. Let\'s have a look.";
+		dialogue[1] = "Profile: April May (F, 23)\nHeight: 157cm.\nAn employee of Bluecorp.\nRelation to case: was in Gatewater Hotel, occupant of Room 303.";
+		dialogue[2] = "Phoenix: S-she\'s kinda cute... I like her pink blouse with heart-shaped buttons... wait.\nI feel like I\'ve seen a button like that before. Could it be..?";
+	}
+	
+	public String getDialogue() {
+		if (dialogueCount >= dialogue.length) isDialogueFinished = true;
+		if (isDialogueFinished) return "-- End of text --\n\nPlease enter a valid command.";
+		else return dialogue[dialogueCount++];
+	}
+	
+	public String getDescription(MazeMaker maze) {
+		StringWriter sw = new StringWriter();
+    	PrintWriter pw = new PrintWriter(sw);
+    	pw.println("August 3, 11:00 AM - Police Station -- Evidence Room");
+    	if (dialogueCount < dialogue.length) pw.println("\nPress enter to advance text.");
+    	
+    	return sw.toString();
+	}
+	
+	public boolean getDialogueStatus() {
+		return isDialogueFinished;
+	}
+	
+	public String getRoomImg() {
+		return "8.png";
+	}
+	
+	@Command(command="use button")
+	public String useButton(MazeMaker maze) {
+		if(!maze.findItem("button")) return "Phoenix: (What button?)";
+		if (usedButton) return "Phoenix: (There's no time to waste, I should use luminol on the button.)";
+		if (sprayedButtonWithLuminol) return "Phoenix: (There's no time to waste, I should go to the murderer's hotel room. It's Room 303, Gatewater Hotel.)";
+		else {
+			usedButton = true;
+			return "Phoenix: Just as I thought... it matches Ms. May\'s blouse buttons. If it was at the crime scene, though... Ah! I see!\n" +
+				"The gap in the blood stain is heart-shaped. It\'s too good to be a coincidence, but maybe I should check that button with Luminol.";
+		}
+	}
+	
+	@Command(command="use luminol")
+	public String useLuminol(MazeMaker maze) {
+		if (usedButton) {
+			if(sprayedButtonWithLuminol) {
+				return "Phoenix: (I've already sprayed the button with luminol.)";
+			} else {
+				if(!maze.findItem("luminol")) return "Phoenix: (What's luminol?)";
+				if(!maze.findItem("button")) return "Phoenix: (What am I supposed to spray this at??)";
+				if(!maze.findItem("button") && !maze.findItem("luminol")) return "Phoenix: (What..?)";
+				
+				sprayedButtonWithLuminol = true;
+				maze.items.remove("button");
+				maze.items.put("bloodyButton", "Pink, heart-shaped, and was drenched in blood.");
+				return "Phoenix: The button has turned a ghastly blue. There\'s no mistaking it. This buttton and its perky owner was at the crime scene. Sorry, Ms. May, but it looks like I\'ll have to visit your room.";
+			}
+		} else {
+			return "Phoenix: (What am I supposed to spray this at??)";
+		}
+	}
 }
--- DragonMazeRunner/src/room/Room9.java
+++ DragonMazeRunner/src/room/Room9.java
@@ -1,5 +1,98 @@
 package room;
 
-public class Room9 {
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
+import anno.Command;
+import anno.Direction;
+import maze.MazeMaker;
+
+public class Room9 {
+	@Direction(command="wrightOffice")
+	private Room1 room1;
+	@Direction(command="grossbergOffice")
+	private Room2 room2;
+	@Direction(command="policeStation")
+	private Room3 room3;
+	@Direction(command="parkingGarage")
+	private Room4 room4;
+	@Direction(command="sideRoom")
+	private Room5 room5;
+	@Direction(command="forestLake")
+	private Room6 room6;
+	@Direction(command="edgeworthOffice")
+	private Room7 room7;
+	@Direction(command="evidenceRoom")
+	private Room8 room8;
+	@Direction(command="court")
+	private Room10 room10;
+	
+	private boolean usedButton, checkedDrawer;
+	
+	private String[] dialogue;
+	private int dialogueCount;
+	public boolean isDialogueFinished;
+	
+	public Room9() {
+		dialogue = new String[1];
+		dialogueCount = 0;
+		isDialogueFinished = false;
+
+		dialogue[0] = "Phoenix: Huh... the door was left open. Quick, I have to find where this button belongs to. Also, there seems to be an open drawer in that cabinet.\n";
+	}
+	
+	public String getDialogue() {
+		if (dialogueCount >= dialogue.length) isDialogueFinished = true;
+		if (isDialogueFinished) return "-- End of text --\n\nPlease enter a valid command.";
+		else return dialogue[dialogueCount++];
+	}
+	
+	public String getDescription(MazeMaker maze) {
+		StringWriter sw = new StringWriter();
+    	PrintWriter pw = new PrintWriter(sw);
+    	pw.println("August 3, 11:45 AM - Gatewater Hotel -- Room 303");
+    	if (dialogueCount < dialogue.length) pw.println("\nPress enter to advance text.");
+    	
+    	return sw.toString();
+	}
+	
+	public boolean getDialogueStatus() {
+		return isDialogueFinished;
+	}
+	
+	public String getRoomImg() {
+		return "9.png";
+	}
+	
+	@Command(command="use bloodyButton")
+	public String useButton(MazeMaker maze) {
+		if (!maze.findItem("bloodyButton") && usedButton) return "Phoenix: (I've already established that the button belongs to the blouse. What other leads are there..?)";
+		else {
+			usedButton = true;
+			maze.items.remove("bloodyButton");
+			maze.items.put("bloodstainedBlouse", "Pink blouse with a missing button and some bloodstains. Thought to belong to April May.");
+			return "Phoenix: The clothes drawer-- aha! A pink blouse with a button missing and a dried red stain! This is all the evidence I need to pin the blame on her.";
+		}
+	}
+	
+	@Command(command="check drawer")
+	public String checkDrawer(MazeMaker maze) {
+		if (checkedDrawer) return "Phoenix: (Take the screwdriver, Phoenix!)";
+		else {
+			checkedDrawer = true;
+			return "Phoenix: There\'s a screwdriver inside. I don\'t know how this is related, but I should take screwdriver just in case.";
+		}
+	}
+	
+	@Command(command="take screwdriver")
+	public String takeScrewdriver(MazeMaker maze) {
+		if (!checkedDrawer) return "What screwdriver?";
+		else {
+			if (maze.findItem("screwdriver")) return "Phoenix: (I've already taken the screwdriver.)";
+			else {
+				maze.items.put("screwdriver", "A standard, flathead screwdriver. Found in April May\'s room.");
+				return "(Screwdriver added to Court Record.)";
+			}
+		}
+	}
 }
