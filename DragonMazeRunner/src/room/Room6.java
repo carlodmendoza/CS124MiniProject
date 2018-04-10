f1ec454ef70e584308bce7b8d6d5a371b6e701f2
gnrlpz <gener.lopez@gmail.com>
2018-04-10 19:57:25 +0800

    added room6 and 7

--- DragonMazeRunner/src/room/Room6.java
+++ DragonMazeRunner/src/room/Room6.java
@@ -1,5 +1,67 @@
 package room;
 
-public class Room6 {
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
+import anno.Command;
+import anno.Direction;
+import maze.MazeMaker;
+
+public class Room6 {
+	@Direction(command="sideRoom")
+	private Room5 room5;
+	@Direction(command="edgeworthOffice")
+	private Room7 room7;
+
+	private String[] dialogue;
+	private int dialogueCount;
+	public boolean isDialogueFinished;
+	
+	public Room6() {
+		dialogue = new String[5];
+		dialogueCount = 0;
+		isDialogueFinished = false;
+		dialogue[0] = "Lotta: Well if it isn\'t that blue hedgehog! What in tarnation are ya doin\' in the middle of nowhere? And what\'s that bag ya got? Hmmm?";
+		dialogue[1] = "Phoenix: Hi, Lotta...Hart. Look, I didn\'t steal your camera, I was just told to give it to you.";
+		dialogue[2] = "Lotta: Give it to me!! My baby...";
+		// [use camera]
+		dialogue[3] = "Lotta: Thanks y\'all. I was just gonna get ready to go over to that red guy\'s office with my camera--wait! Forget I said that! So long, gotta get back to the heart of the heartland!!";
+		dialogue[4] = "Phoenix: ...she ran away fast. She mentioned something about, I guess, edgeworthOffice? Would that be my next lead?";
+	}
+	
+	public String getDescription(MazeMaker maze) {
+		StringWriter sw = new StringWriter();
+    	PrintWriter pw = new PrintWriter(sw);
+    	pw.println("August 3, 10:20 AM - Gourd Lake Forest");
+    	if (dialogueCount < dialogue.length) pw.println("\nPress enter to advance text.");
+    	
+    	return sw.toString();
+	}
+	
+	public String getDialogue() {
+		if (dialogueCount >= dialogue.length || dialogueCount == 3) isDialogueFinished = true;
+		if (isDialogueFinished) return "-- End of text --\n\nPlease enter a valid command.";
+		else return dialogue[dialogueCount++];
+	}
+	
+	public boolean getDialogueStatus() {
+		return isDialogueFinished;
+	}
+	
+	public String getRoomImg() {
+		if (dialogueCount >= 5) return "6.png";
+		if (dialogueCount >= 4) return "6-lottaok.png";
+		if (dialogueCount >= 0) return "6-lottamad.png";
+		else return "6.png";
+	}
+	
+	@Command(command="give camera")
+	public String giveCamera(MazeMaker maze) {
+		maze.items.remove("camera");
+		if(dialogueCount >= dialogue.length) {
+			if(!maze.findItem("camera")) return "Phoenix: (I already gave her the camera...)";
+			return getDialogue();
+		}
+    	return dialogue[dialogueCount++];
+    }
 }
--- DragonMazeRunner/src/room/Room7.java
+++ DragonMazeRunner/src/room/Room7.java
@@ -1,5 +1,57 @@
 package room;
 
-public class Room7 {
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
+import anno.Command;
+import anno.Direction;
+import maze.MazeMaker;
+
+public class Room7 {
+	@Direction(command="forestLake")
+	private Room6 room6;
+	@Direction(command="")
+	private Room8 room8;
+
+	private String[] dialogue;
+	private int dialogueCount;
+	public boolean isDialogueFinished;
+	
+	public Room7() {
+		dialogue = new String[2];
+		dialogueCount = 0;
+		isDialogueFinished = false;
+		dialogue[0] = "Phoenix: Oh, looks like Edgeworth isn\'t here. I wanted to see him today. I guess I can check back later--wait, I wonder what\'s that on his desk?";
+		dialogue[1] = "It looks like there\'s some sort of key on his desk. It\'s labelled \"Evidence Room (evidenceKey).\"";
+	}
+	
+	public String getDialogue() {
+		if (dialogueCount >= dialogue.length) isDialogueFinished = true;
+		if (isDialogueFinished) return "-- End of text --\n\nPlease enter a valid command.";
+		else return dialogue[dialogueCount++];
+	}
+	
+	public String getDescription(MazeMaker maze) {
+		StringWriter sw = new StringWriter();
+    	PrintWriter pw = new PrintWriter(sw);
+    	pw.println("August 3, 10:45 AM - High Prosecutor Office");
+    	if (dialogueCount < dialogue.length) pw.println("\nPress enter to advance text.");
+    	
+    	return sw.toString();
+	}
+	
+	public boolean getDialogueStatus() {
+		return isDialogueFinished;
+	}
+	
+	public String getRoomImg() {
+		return "7.png";
+	}
+	
+	@Command(command="take evidenceKey")
+	public String takeButton(MazeMaker maze) {
+		if(maze.findItem("button")) return "Phoenix: (I already took the key...)";
+		maze.items.put("evidenceKey", "Key to the police station evidence room."); 
+		return "Phoenix: Sorry, Edgeworth. Looks like I\'ll be borrowing this. (Looks like I can now goto evidenceRoom.)";
+	}
 }
